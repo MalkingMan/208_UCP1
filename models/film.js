590875f86960e2b113b412d6e32489daa3157864
MalkingMan <arrayalkhozini@gmail.com>
2025-11-04 09:25:58 +0700

    Perbaikan

--- models/film.js
+++ models/film.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const Komik = sequelize.define('Komik', {
+  const Film = sequelize.define('Film', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
