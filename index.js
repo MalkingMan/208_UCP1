5c6b65df83b9681daea27f2e5baf87d2b37ee1d6
MalkingMan <arrayalkhozini@gmail.com>
2025-11-04 10:00:11 +0700

    Perbaikan

--- index.js
+++ index.js
@@ -1,70 +1,75 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
-const port = 3000
+const port = 3309;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, async () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
 
 db.sequelize.sync()
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
+    app.listen(port);
   })
   .catch((err) => {
     console.error('Failed to sync database:', err);
   });
 
-  app.get('/komik', async (req, res) => {
+  app.get('/film', async (req, res) => {
     try {
-        const komik = await db.Komik.findAll();
-        res.json(komik);
+        const film = await db.Film.findAll();
+        res.json(film);
     } catch (err) {
         res.send(err);
     }
   });
 
-    app.post('/komik', async (req, res) => {
+  app.get('/film', async (req, res) => {
+    try {
+        const film = await db.Film.findAll();
+        res.json(film);
+    } catch (err) {
+        res.send(err);
+    }
+  });
+
+    app.post('/film', async (req, res) => {
     const data = req.body;
     try {
-        const komik = await db.Komik.create(data);
-        res.send(komik);
+        const film = await db.Film.create(data);
+        res.send(film);
     } catch (err) {
         res.send(err);
     }
  });
 
- app.put('/komik/:id', async (req, res) => {
+ app.put('/film/:id', async (req, res) => {
     const id = req.params.id;
     const data = req.body;
 
     try {
-        const komik = await db.Komik.findByPk(id);
-        if (!komik) {
-            return res.status(404).send({ message: 'Komik not found' });
+        const film = await db.Film.findByPk(id);
+        if (!film) {
+            return res.status(404).send({ message: 'Film not found' });
         }
-        await komik.update(data);
-        res.send({ message: 'Komik updated successfully', komik });
+        await film.update(data);
+        res.send({ message: 'Film updated successfully', film });
     } catch (err) {
         res.status(500).send(err);
     }
  });
 
-app.delete('/komik/:id', async (req, res) => {
+app.delete('/film/:id', async (req, res) => {
     const id  = req.params.id;
     try {
-        const komik = await db.Komik.findByPk(id);
-        if (!komik) {
-            return res.status(404).send({ message: 'Komik not found' });
+        const film = await db.Film.findByPk(id);
+        if (!film) {
+            return res.status(404).send({ message: 'Film not found' });
         }
 
-        await komik.destroy();
-        res.send({ message: 'Komik deleted successfully' });
+        await film.destroy();
+        res.send({ message: 'Film deleted successfully' });
     } catch (err) {
         res.status(500).send(err);
     }
